cf848515ed64c276390a36f9642516ec16cdfdc5
benhjt <benhjt@users.noreply.github.com>
2016-03-09 11:30:54 +0000

    Initial commit

---
+++ src/rough-time.js
@@ -0,0 +1,79 @@
+'use strict';
+
+class RoughTime {
+	constructor() {}
+
+	getRoughTime(date = new Date()) {
+		if (typeof date !== 'object' || !(date instanceof Date)) {
+			throw new Error('Date must be a JavaScript Date Object');
+		}
+
+		let roughTimeString = 'it is ';
+		let minutes = date.getMinutes();
+
+		if (minutes <= 3) {
+			roughTimeString += this._getHoursString(date.getHours());
+			roughTimeString += ' o\'clock';
+		} else {
+			roughTimeString += this._getMinutesString(minutes);
+			roughTimeString += this._getMinutesSuffix(minutes);
+			roughTimeString += this._getHoursString(date.getHours());
+		}
+
+		return roughTimeString;
+	}
+
+	_getMinutesString(minutes) {
+		if ((minutes >= 5 && minutes <= 9) || (minutes >= 55 && minutes <= 59)) {
+			return 'five';
+		} else if ((minutes >= 10 && minutes <= 14) || (minutes >= 50 && minutes <= 54)) {
+			return 'ten';
+		} else if ((minutes >= 15 && minutes <= 19) || (minutes >= 45 && minutes <= 49)) {
+			return 'quarter';
+		} else if ((minutes >= 20 && minutes <= 24) || (minutes >= 40 && minutes <= 44)) {
+			return 'twenty';
+		} else if ((minutes >= 25 && minutes <= 29) || (minutes >= 35 && minutes <= 40)) {
+			return 'twentyfive';
+		} else {
+			return 'half';
+		}
+	}
+
+	_getMinutesSuffix(minutes) {
+		if (minutes <= 34) {
+			return ' past ';
+		} else {
+			return ' to ';
+		}
+	}
+
+	_getHoursString(hours) {
+		if (hours === 1 || hours === 13) {
+			return 'one';
+		} else if (hours === 2 || hours === 14) {
+			return 'two';
+		} else if (hours === 3 || hours === 15) {
+			return 'three';
+		} else if (hours === 4 || hours === 16) {
+			return 'four';
+		} else if (hours === 5 || hours === 17) {
+			return 'five';
+		} else if (hours === 6 || hours === 18) {
+			return 'six';
+		} else if (hours === 7 || hours === 19) {
+			return 'seven';
+		} else if (hours === 8 || hours === 20) {
+			return 'eight';
+		} else if (hours === 9 || hours === 21) {
+			return 'nine';
+		} else if (hours === 10 || hours === 22) {
+			return 'ten';
+		} else if (hours === 11 || hours === 23) {
+			return 'eleven';
+		} else {
+			return 'twelve';
+		}
+	}
+}
+
+export default RoughTime;
---
+++ test/rough-time.spec.js
@@ -0,0 +1,150 @@
+'use strict';
+
+import RoughTime from '../src/rough-time.js';
+import chai from 'chai';
+
+let expect = chai.expect;
+let assert = chai.assert;
+
+describe('RoughTime (unit)', () => {
+  describe('when instantiated', () => {
+    let roughtime = new RoughTime();
+    it('should return an Object', () => {
+      expect(roughtime).to.be.an('object');
+    });
+  });
+
+  describe('getRoughTime', () => {
+    let roughtime = new RoughTime();
+    it('should be a Function', () => {
+      expect(roughtime.getRoughTime).to.be.a('function');
+    });
+
+    it('should return a String', () => {
+      expect(roughtime.getRoughTime()).to.be.a('string');
+    });
+
+    it('should throw an error if the argument is not a Date object', () => {
+      expect(roughtime.getRoughTime.bind(undefined)).to.throw(Error);
+      expect(roughtime.getRoughTime.bind(null)).to.throw(Error);
+      expect(roughtime.getRoughTime.bind('bob')).to.throw(Error);
+      expect(roughtime.getRoughTime.bind(2345)).to.throw(Error);
+      expect(roughtime.getRoughTime.bind(false)).to.throw(Error);
+      expect(roughtime.getRoughTime.bind([1, 2, 3, 4])).to.throw(Error);
+      expect(roughtime.getRoughTime.bind({
+        name: 'Bob'
+      })).to.throw(Error);
+    });
+
+    it('should default to current time if a time is not entered', () => {
+      assert.equal(roughtime.getRoughTime(), roughtime.getRoughTime(new Date()));
+    });
+  });
+
+  describe('_getHoursString', () => {
+    let roughtime = new RoughTime();
+    it('should be a Function', () => {
+      expect(roughtime._getHoursString).to.be.a('function');
+    });
+
+    it('should return a String', () => {
+      expect(roughtime._getHoursString()).to.be.a('string');
+    });
+
+    it('should return "one" when the hour is 1 or 13', () => {
+      assert.equal(roughtime._getHoursString(1), 'one');
+      assert.equal(roughtime._getHoursString(13), 'one');
+    });
+
+    it('should return "two" when the hour is 2 or 14', () => {
+      assert.equal(roughtime._getHoursString(2), 'two');
+      assert.equal(roughtime._getHoursString(14), 'two');
+    });
+
+    it('should return "two" when the hour is 3 or 15', () => {
+      assert.equal(roughtime._getHoursString(3), 'three');
+      assert.equal(roughtime._getHoursString(15), 'three');
+    });
+
+    it('should return "two" when the hour is 4 or 16', () => {
+      assert.equal(roughtime._getHoursString(4), 'four');
+      assert.equal(roughtime._getHoursString(16), 'four');
+    });
+
+    it('should return "two" when the hour is 5 or 17', () => {
+      assert.equal(roughtime._getHoursString(5), 'five');
+      assert.equal(roughtime._getHoursString(17), 'five');
+    });
+
+    it('should return "two" when the hour is 6 or 18', () => {
+      assert.equal(roughtime._getHoursString(6), 'six');
+      assert.equal(roughtime._getHoursString(18), 'six');
+    });
+
+    it('should return "two" when the hour is 7 or 19', () => {
+      assert.equal(roughtime._getHoursString(7), 'seven');
+      assert.equal(roughtime._getHoursString(19), 'seven');
+    });
+
+    it('should return "two" when the hour is 8 or 20', () => {
+      assert.equal(roughtime._getHoursString(8), 'eight');
+      assert.equal(roughtime._getHoursString(20), 'eight');
+    });
+
+    it('should return "two" when the hour is 9 or 21', () => {
+      assert.equal(roughtime._getHoursString(9), 'nine');
+      assert.equal(roughtime._getHoursString(21), 'nine');
+    });
+
+    it('should return "two" when the hour is 10 or 22', () => {
+      assert.equal(roughtime._getHoursString(10), 'ten');
+      assert.equal(roughtime._getHoursString(22), 'ten');
+    });
+
+    it('should return "two" when the hour is 11 or 23', () => {
+      assert.equal(roughtime._getHoursString(11), 'eleven');
+      assert.equal(roughtime._getHoursString(23), 'eleven');
+    });
+
+    it('should return "two" when the hour is 0 or 12', () => {
+      assert.equal(roughtime._getHoursString(0), 'twelve');
+      assert.equal(roughtime._getHoursString(12), 'twelve');
+    });
+  });
+
+  describe('_getMinutesSuffix', () => {
+    let roughtime = new RoughTime();
+    it('should be a Function', () => {
+      expect(roughtime._getMinutesSuffix).to.be.a('function');
+    });
+
+    it('should return a String', () => {
+      expect(roughtime._getMinutesSuffix()).to.be.a('string');
+    });
+
+    for (let i = 0; i < 50; i++) {
+      let randomMinute = Math.floor(Math.random() * 34) + 1;
+      it('should return " past " when minutes is ' + randomMinute, () => {
+        assert.equal(roughtime._getMinutesSuffix(randomMinute), ' past ');
+      });
+    }
+
+    for (let i = 0; i < 50; i++) {
+      let randomMinute = Math.floor(Math.random() * 59) + 35;
+      it('should return " to " when minutes is ' + randomMinute, () => {
+        assert.equal(roughtime._getMinutesSuffix(randomMinute), ' to ');
+      });
+    }
+  });
+
+  describe('_getMinutesString', () => {
+    let roughtime = new RoughTime();
+    it('should be a Function', () => {
+      expect(roughtime._getMinutesString).to.be.a('function');
+    });
+
+    it('should return a String', () => {
+      expect(roughtime._getMinutesString()).to.be.a('string');
+    });
+  });
+});
